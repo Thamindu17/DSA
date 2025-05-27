845eb0040641763936a2d0a79e45a8a8c856afd3
Thamindu17 <166316896+Thamindu17@users.noreply.github.com>
2025-05-28 01:08:46 +0530

    m

--- BinaryTree.java
+++ BinaryTree.java
@@ -1,78 +1,189 @@
-class Node{
-    int data;
-    Node leftNode;
-    Node rightNode;
+import java.util.Scanner;
 
-    public Node(int data) {
-        this.data = data;
-       
-    }
-
-}
-
-
-
+// Main class
 public class BinaryTree {
-    
-    Node rootNode;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-    public void insert(int value) {
-       rootNode=insertRec(rootNode, value);
-    }
+        BinarySearchTree bst = new BinarySearchTree();
 
+        System.out.println("Enter keys to insert into the BST (Enter 'END' to finish):");
+        String line = sc.nextLine();
 
-    public Node insertRec(Node rootNode , int value){
-        if(rootNode == null){
-            rootNode = new Node(value);
-            
+        // Accept inputs until the user enters END
+        while (!line.equalsIgnoreCase("END")) {
+            try {
+                int value = Integer.parseInt(line);
+                bst.insert(value);
+            } catch (NumberFormatException e) {
+                System.out.println("Please enter a valid number.");
+            }
+            System.out.println("Enter next key (Enter 'END' to finish):");
+            line = sc.nextLine();
         }
-        else if(rootNode.data < value){
-            rootNode.rightNode = insertRec(rootNode.rightNode, value);
-        }
-        else if(rootNode.data > value){
-            rootNode.leftNode = insertRec(rootNode.leftNode, value);
-        }
 
-        return rootNode;
+        // Sample tree operations
+        System.out.println("\n--- Tree Traversals ---");
+        System.out.print("Inorder Traversal: ");
+        bst.inorder();
+
+        System.out.print("\nPreorder Traversal: ");
+        bst.preorder();
+
+        System.out.print("\nPostorder Traversal: ");
+        bst.postorder();
+
+        // Search demo
+        System.out.print("\n\nEnter key to search: ");
+        int keyToSearch = sc.nextInt();
+        boolean found = bst.search(keyToSearch);
+        System.out.println(found ? "Key found!" : "Key not found.");
+
+        // Delete demo
+        System.out.print("\nEnter key to delete: ");
+        int keyToDelete = sc.nextInt();
+        bst.delete(keyToDelete);
+
+        System.out.println("Inorder traversal after deletion:");
+        bst.inorder();
     }
-
-
-
-
-    public void preorder() {
-        System.out.println("/n");
-        preorderRec(rootNode);
-    }
-
-
-    
-
-    public void preorderRec(Node rootNode){
-        if(rootNode != null){
-
-          System.out.print(rootNode.data + " ");
-          preorderRec(rootNode.leftNode);  
-          
-          preorderRec(rootNode.rightNode);
-          
-        }
-    }
-
-
-    public void inorder() {
-        inorderRec(rootNode);
-    }
-
-    public void inorderRec(Node rootNode){
-        if(rootNode != null){
-          inorderRec(rootNode.leftNode);  
-          System.out.print(rootNode.data + " ");
-          inorderRec(rootNode.rightNode);
-          
-        }
-    }
-
-
 }
 
+// Node class (represents each node in the tree)
+class TreeNode {
+    int key;
+    TreeNode left, right;
 
+    public TreeNode(int key) {
+        this.key = key;
+        left = right = null;
+    }
+}
+
+// Binary Search Tree class
+class BinarySearchTree {
+    TreeNode root;
+
+    public BinarySearchTree() {
+        root = null;
+    }
+
+    // Insert operation
+    public void insert(int key) {
+        root = insertRec(root, key);
+    }
+
+    private TreeNode insertRec(TreeNode root, int key) {
+        if (root == null) {
+            System.out.println("Inserting " + key);
+            return new TreeNode(key);
+        }
+
+        if (key < root.key) {
+            System.out.println("Moving left of " + root.key);
+            root.left = insertRec(root.left, key);
+        } else if (key > root.key) {
+            System.out.println("Moving right of " + root.key);
+            root.right = insertRec(root.right, key);
+        } else {
+            System.out.println("Key already exists.");
+        }
+
+        return root;
+    }
+
+    // Search operation
+    public boolean search(int key) {
+        return searchRec(root, key);
+    }
+
+    private boolean searchRec(TreeNode root, int key) {
+        if (root == null) return false;
+
+        if (key == root.key) return true;
+
+        return key < root.key ? searchRec(root.left, key) : searchRec(root.right, key);
+    }
+
+    // Delete operation
+    public void delete(int key) {
+        root = deleteRec(root, key);
+    }
+
+    private TreeNode deleteRec(TreeNode root, int key) {
+        if (root == null) return null;
+
+        if (key < root.key) {
+            root.left = deleteRec(root.left, key);
+        } else if (key > root.key) {
+            root.right = deleteRec(root.right, key);
+        } else {
+            // Case 1: No child
+            if (root.left == null && root.right == null) {
+                return null;
+            }
+            // Case 2: One child
+            else if (root.left == null) {
+                return root.right;
+            } else if (root.right == null) {
+                return root.left;
+            }
+            // Case 3: Two children
+            else {
+                TreeNode successor = findMin(root.right);
+                root.key = successor.key;
+                root.right = deleteRec(root.right, successor.key);
+            }
+        }
+        return root;
+    }
+
+    // Helper to find minimum node (used in delete)
+    private TreeNode findMin(TreeNode node) {
+        while (node.left != null)
+            node = node.left;
+        return node;
+    }
+
+    // Inorder traversal (Left, Root, Right)
+    public void inorder() {
+        inorderRec(root);
+        System.out.println();
+    }
+
+    private void inorderRec(TreeNode root) {
+        if (root != null) {
+            inorderRec(root.left);
+            System.out.print(root.key + " ");
+            inorderRec(root.right);
+        }
+    }
+
+    // Preorder traversal (Root, Left, Right)
+    public void preorder() {
+        preorderRec(root);
+        System.out.println();
+    }
+
+    private void preorderRec(TreeNode root) {
+        if (root != null) {
+            System.out.print(root.key + " ");
+            preorderRec(root.left);
+            preorderRec(root.right);
+        }
+    }
+
+    // Postorder traversal (Left, Right, Root)
+    public void postorder() {
+        postorderRec(root);
+        System.out.println();
+    }
+
+    private void postorderRec(TreeNode root) {
+        if (root != null) {
+            postorderRec(root.left);
+            postorderRec(root.right);
+            System.out.print(root.key + " ");
+        }
+    }
+}
